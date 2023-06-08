40dae7289970517edeef10677bcdbab8040dd7fd
Yakun Hu <116873082+yakun-hu@users.noreply.github.com>
2023-06-08 13:11:46 -0400

    Create custom.JS-contr,media.query-test.php

---
+++ testing-progress/Framework-testing/custom.JS-contr,media.query-test.php
@@ -0,0 +1,7 @@
+Using the DOM-over,r in backlog-manager on # inher edit.panel-inc abs-pos[dash-v], snip:
+<script>document.getElementById("bold").style = "position:absolute;top:348px;left:172px;background-color:black;height:30px;color:#FFFFFF";</script>
+I can set over-r using a cond-contr by https://www.w3schools.com/jsref/prop_nav_useragent.asp
+
+For-ex, in media-player.php, this over-r can affect the iframe.width-100% for device types = "mobile"
+
+<!-- http://flare/testing-progress/Framework-testing/custom.JS-contr,media.query-test.php -->
